098b77630fce0e06217eb20707b9bdbe17dec076
SorryKim <06zzkimzz06@gmail.com>
2022-07-21 19:15:56 +0900

    2022-07-21
    상하좌우 이동 구현, 게임 오브젝트 조사 액션 구현

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -7,23 +7,88 @@ public class PlayerAction : MonoBehaviour
     Rigidbody2D rigid;
     float h, v;
     public float Speed;
+    bool isHorizonMove;
+    Animator anim;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move value
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        // 대각선 이동을 제한하여 상하좌우로만 이동을 가능하게 함
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check Horizontal Move
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = (h != 0);
+
+        // Direction
+        if(vDown && v == 1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+
+
+
+        // Animation
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+            anim.SetBool("isChange", false);
+
+        // Scan Object
+        if (Input.GetButtonDown("Jump") && scanObject != null) {
+            Debug.Log("this is :" + scanObject.name);
+        }
+
     }
 
     private void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v) * Speed;
+        // Move
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
+
+        // Ray
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
     }
 
 
